46973b00536e762a49fca296784c70ffa2bc3f2e
Tang Wai Hoh <tang.waihoh@u.nus.edu>
2020-11-18 15:32:48 +0800

    removed intermediate working files.

--- ctgan/__init__.py
+++ ctgan/__init__.py
@@ -9,17 +9,11 @@ __version__ = '0.2.3.dev0'
 from ctgan.demo import load_demo
 from ctgan.synthesizer import CTGANSynthesizer
 from ctgan.tvae import TVAESynthesizer
-from ctgan.tvae_original import TVAESynthesizerOriginal
 from ctgan.tablegan import TableganSynthesizer
-from ctgan.tablegan_wo_cond_gen import TableganSynthesizerWOCondGen
-from ctgan.tablegan_original import TableganSynthesizerOriginal
 
 __all__ = (
     'CTGANSynthesizer',
     'TVAESynthesizer',
-    'TVAESynthesizerOriginal',
     'TableganSynthesizer',
-    'TableganSynthesizerWOCondGen',
-    'TableganSynthesizerOriginal',
     'load_demo'
 )
