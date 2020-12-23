5d48c40f0b7e3e438a1c880326f9c3f2694277bf
Tianming <u0903411@gmail.com>
2020-12-23 17:41:48 +0800

    comment validation parts to save time

--- ctgan/tablegan.py
+++ ctgan/tablegan.py
@@ -357,13 +357,13 @@ class TableganSynthesizer(object):
         self.classifier.apply(weights_init)
 
         steps_per_epoch = max(len(train_data) // self.batch_size, 1)
-        self.threshold = M.determine_threshold(train_data0, val_data.shape[0], discrete_columns,
-                                               n_rep=1000)
-        print(self.threshold)
-        self.train_KLD = []
-        self.prop_dis_train = []
-        self.validation_KLD = []
-        self.prop_dis_validation = []
+        # self.threshold = M.determine_threshold(train_data0, val_data.shape[0], discrete_columns,
+        #                                        n_rep=1000)
+        # print(self.threshold)
+        # self.train_KLD = []
+        # self.prop_dis_train = []
+        # self.validation_KLD = []
+        # self.prop_dis_validation = []
         for i in range(epochs):
             self.generator.train()  ##switch to train mode
             self.trained_epoches += 1
@@ -444,16 +444,16 @@ class TableganSynthesizer(object):
                   (self.trained_epoches, loss_g.detach().cpu(), loss_d.detach().cpu()),
                    flush=True)
             ## synthetic data by the generator for each epoch
-            sampled_train = self.sample(val_data.shape[0], condition_column=None,
-                                            condition_value=None)
-            KL_val_loss = M.KLD(val_data, sampled_train, discrete_columns)
-            KL_train_loss = M.KLD(train_data0, sampled_train, discrete_columns)
-            diff_train = KL_train_loss - self.threshold
-            diff_val = KL_val_loss - self.threshold
-            self.train_KLD.append(KL_train_loss)
-            self.validation_KLD.append(KL_val_loss)
-            self.prop_dis_train.append(np.count_nonzero(diff_train >= 0) / np.count_nonzero(~np.isnan(diff_train)))
-            self.prop_dis_validation.append(np.count_nonzero(diff_val >= 0) / np.count_nonzero(~np.isnan(diff_val)))
+            # sampled_train = self.sample(val_data.shape[0], condition_column=None,
+            #                                 condition_value=None)
+            # KL_val_loss = M.KLD(val_data, sampled_train, discrete_columns)
+            # KL_train_loss = M.KLD(train_data0, sampled_train, discrete_columns)
+            # diff_train = KL_train_loss - self.threshold
+            # diff_val = KL_val_loss - self.threshold
+            # self.train_KLD.append(KL_train_loss)
+            # self.validation_KLD.append(KL_val_loss)
+            # self.prop_dis_train.append(np.count_nonzero(diff_train >= 0) / np.count_nonzero(~np.isnan(diff_train)))
+            # self.prop_dis_validation.append(np.count_nonzero(diff_val >= 0) / np.count_nonzero(~np.isnan(diff_val)))
 
     ### following ctgan and tvae, added the parts updated by the authors.
     def sample(self, n, condition_column=None, condition_value=None):
