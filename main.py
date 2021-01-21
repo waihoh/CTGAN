dde0d87c9c5bc520fd8ab495264600a078cc8f5b
Tang Wai Hoh <waihoh@gmail.com>
2021-01-21 17:11:22 +0800

    generate sample csv when running main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import os
 import time
 import torch
 import numpy as np
+import pandas as pd
 
 from ctgan.argparser import ParserOutput
 from ctgan import CTGANSynthesizer
@@ -57,7 +57,16 @@ if parser.proceed:
     elapsed_time = time.time() - start_time
     model.logger.write_to_file("Training time {:.2f} seconds".format(elapsed_time), True)
 
+    # Use same PID as log file for sample csv and the trained model pkl files.
+    PIDTAG = model.logger.PID + "_" + model.logger.dt.now().strftime(model.logger.datetimeformat)
+
+    # generate synthetic data
+    sample_fn = parser.model_type + "_sample_" + PIDTAG + ".csv"
+    output_sample_path = os.path.join(model.logger.dirpath, sample_fn)
+    samples = model.sample(parser.samplesize, condition_column=None, condition_value=None)
+    samples.to_csv(output_sample_path, index=False, header=True)
+
     # Save the model in the same folder as the log file
-    model_fn = parser.model_type + "_" + model.logger.PID + "_" + model.logger.now.strftime(model.logger.datetimeformat) + ".pkl"
+    model_fn = parser.model_type + "_" + PIDTAG + ".pkl"
     output_model_path = os.path.join(model.logger.dirpath, model_fn)
     model.save(output_model_path)
